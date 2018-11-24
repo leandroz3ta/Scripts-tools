4ed278aa19625a3d19ac8131ec01328270052599
Leandro Z3ta <leandroz3ta@gmail.com>
2018-11-24 09:56:40 -0200

    Nova versao

---
+++ OrangePiEssentials2.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+echo "Selecione uma das opcoes abaixo:";
+echo "1) Instalacao pacotes basicos de ambiente.";
+echo "2) Instalacao instalar sistema na memoria interna.";
+echo "3) Instalacao pacotes I2c.";
+
+echo "1) Instalacao pacotes basicos de ambiente.";
+echo "1) Instalacao pacotes basicos de ambiente.";
+while :
+do
+  read INPUT_STRING
+  case $INPUT_STRING in
+	1)
+		apt-get update;
+    apt-get upgrade;
+    apt-get install nano;
+    apt-get install wpa_supplicant;
+    echo "Pacotes basicos instalados.";
+		;;
+	2)
+    echo "Instalando sistema na memoria interna do dispositivo...";
+    nand-sata-install;
+		;;
+	3)
+        echo "Instalando pacotes I2c..."
+        apt-get install python-smbus python-dev;
+        apt-get install i2c-tools;
+        echo "Habilitando i2c...";
+        echo "i2c-dev" >> /etc/modules;
+        echo "Adicinando user I2c...";
+        adduser pi i2c;
+        echo "Reiniciando sistema...";
+        reboot;
+		;;
+	*)
+		echo "Opcao invalida!"
+		;;
+  esac
+done
+echo 
+echo "Fim."
