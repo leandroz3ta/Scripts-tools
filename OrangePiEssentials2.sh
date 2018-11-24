15e3a2f59cc5881f65c6c0938e1078641225136b
Leandro Z3ta <leandroz3ta@gmail.com>
2018-11-24 10:11:55 -0200

    Update OrangePiEssentials2.sh

--- OrangePiEssentials2.sh
+++ OrangePiEssentials2.sh
@@ -3,35 +3,32 @@
 echo "Selecione uma das opcoes abaixo:";
 echo "1) Instalacao pacotes basicos de ambiente.";
 echo "2) Instalacao instalar sistema na memoria interna.";
-echo "3) Instalacao pacotes I2c.";
-
-echo "1) Instalacao pacotes basicos de ambiente.";
-echo "1) Instalacao pacotes basicos de ambiente.";
+echo "3) Instalacao pacotes I2c.
 while :
 do
   read INPUT_STRING
   case $INPUT_STRING in
 	1)
 		apt-get update;
-    apt-get upgrade;
-    apt-get install nano;
-    apt-get install wpa_supplicant;
-    echo "Pacotes basicos instalados.";
+    		apt-get upgrade;
+    		apt-get install nano;
+    		apt-get install wpa_supplicant;
+    		echo "Pacotes basicos instalados.";
 		;;
 	2)
-    echo "Instalando sistema na memoria interna do dispositivo...";
-    nand-sata-install;
+    		echo "Instalando sistema na memoria interna do dispositivo...";
+    		nand-sata-install;
 		;;
 	3)
-        echo "Instalando pacotes I2c..."
-        apt-get install python-smbus python-dev;
-        apt-get install i2c-tools;
-        echo "Habilitando i2c...";
-        echo "i2c-dev" >> /etc/modules;
-        echo "Adicinando user I2c...";
-        adduser pi i2c;
-        echo "Reiniciando sistema...";
-        reboot;
+		echo "Instalando pacotes I2c..."
+		apt-get install -y python-smbus python-dev;
+		apt-get install -y i2c-tools;
+		echo "Habilitando i2c...";
+		echo "i2c-dev" >> /etc/modules;
+		echo "Adicinando user I2c...";
+		adduser orangepi i2c;
+		echo "Reiniciando sistema...";
+		reboot;
 		;;
 	*)
 		echo "Opcao invalida!"
