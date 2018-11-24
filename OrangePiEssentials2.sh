fb89e5747226347948bd368d73ffa583326a0bf1
Leandro Z3ta <leandroz3ta@gmail.com>
2018-11-24 10:46:00 -0200

    Update OrangePiEssentials2.sh

--- OrangePiEssentials2.sh
+++ OrangePiEssentials2.sh
@@ -3,7 +3,8 @@
 echo "Selecione uma das opcoes abaixo:";
 echo "1) Instalacao pacotes basicos de ambiente.";
 echo "2) Instalacao instalar sistema na memoria interna.";
-echo "3) Instalacao pacotes I2c.
+echo "3) Instalacao pacotes I2c.";
+echo "4) Habilitar acesso serial via OTG.";
 echo "0) Finalizar assistende de conficuracao";
 while :
 do
@@ -21,6 +22,20 @@ do
     		nand-sata-install;
 		;;
 	3)
+		echo "Habilitando entrada g_serial no arquivo modules..."
+		echo "g_serial" >> /etc/modules;
+		echo "Criando diretorios necessarios...";
+		mkdir -p /etc/systemd/system/serial-getty@ttyGS0.service.d;
+		
+		apt-get install -y i2c-tools;
+		echo "Habilitando i2c...";
+		echo "i2c-dev" >> /etc/modules;
+		echo "Adicinando user I2c...";
+		adduser orangepi i2c;
+		echo "Reiniciando sistema...";
+		reboot;
+		;;
+	4)
 		echo "Instalando pacotes I2c..."
 		apt-get install -y python-smbus python-dev;
 		apt-get install -y i2c-tools;
@@ -31,7 +46,7 @@ do
 		echo "Reiniciando sistema...";
 		reboot;
 		;;
-	3)
+	0)
 		echo "Ate logo! =)"
 		break
 		;;
