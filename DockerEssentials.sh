38f2fa74903fa09e900bd3de09ecd7fa1c6bd80f
Leandro Z3ta <leandroz3ta@gmail.com>
2018-12-08 12:21:13 -0200

    Update DockerEssentials.sh

--- DockerEssentials.sh
+++ DockerEssentials.sh
@@ -9,6 +9,7 @@ figlet Docker Essentials Image v. 1.0
 echo "Selecione o tipo de ambiente a ser instalado:";
 echo "1) Ambiente web.";
 echo "2) Ambiente de testes automatizados.";
+echo "3) Comandos basicos.";
 echo "0) Finalizar assistende de conficuracao";
 while :
 do
@@ -24,6 +25,16 @@ do
     echo "iniciando download da imagem...";
     docker run -p 8080:8080 -p 50000:50000 jenkins;
     echo "Imagem instalada com sucesso!";
+		echo "Digite 0 para sair ou 9 para menu Inicial.";
+		;;
+	3)
+    echo "docker images";
+    echo "docker rm ID_ou_nome_do_container - Remove o container solicitado";
+    echo "docker rmi ID_ou_nome_da_imagem - Remove a imagem solicitada";
+    echo "docker run nome_da_imagem - Instala container a partir da imagem informada";
+    echo "docker run --name ubuntinho ubuntu - Instala o container e o apelida";
+    echo "docker run -it ubuntu /bin/bash - Instala o container e loga no terminal";
+    echo "docker ps -a - Lista todos os containers";
 		echo "Digite 0 para sair ou 9 para menu Inicial.";
 		;;
 	9)
